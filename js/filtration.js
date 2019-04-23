d3e1b60c1bba66913968803813cfd33f4c08cd3f
mambalina <48771321+mambalina@users.noreply.github.com>
2019-04-23 20:09:25 +0300

    Add files via upload

--- js/filtration.js
+++ js/filtration.js
@@ -2,11 +2,28 @@
 $(document).ready(function () {
 
     // отображение товаров при загрузке
+    // var url = $(document).URL;
+    // var regV = "gender";
+    // var pos = url.indexOf(regV);
+    // if (pos == -1){
+    //
+    // }
+    // else{
+    //     alert("sf");
+    // }
+
+    if (window.location.href.indexOf("gender") > -1){
+        var url = window.location.href;
+        var gen = url.charAt(url.length-1);
+        // alert(gen);
+    }
+
     $.ajax({
         type: "post",
         url: "core.php",
         data: {
             "action": "selectAllGoods",
+            "gen" : gen
         },
         success: function (data) {
             data = JSON.parse(data);
@@ -14,97 +31,107 @@ $(document).ready(function () {
             for (var i = 0; i<5; i++){
                 switch (i) {
                     case 0: {
-                        out += "<div class=\"cards\">";
-                        out +=    "<div class=\"col-md-4 col-lg-4 col-xl-4 mx-auto mt-3\">";
-                        out +=    "<div class=\"card\">";
-                        out +=    " <img src=\"img/"+data[i].preview+"\" class=\"card-img-top\" alt=\"...\">";
-                        out +=    "<div class=\"card-body\">";
-                        out +=    "<p class=\"card-text\">"+data[i].name+"</p>";
-                        out +=   "<p class=\"card-text money\">"+data[i].price+" грн.</p>";
-                        out +="<div class=\"buttons\">";
-                        out +=" <a href='information.php?id="+data[i].id+"'><input type=\"button\" class=\"btn sbtn\" value=\"Подробнее\" data-id="+data[i].id+"' ></a>";
-                        out +="<div class=\"input-image\"><input type=\"image\" src=\"img/heart.png\" onclick='checkRegistration()'></div>";
-                        out +=" </div>";
-                        out +=   "</div>";
-                        out +=   "</div>";
-                        out +=    "</div>";
-                        out +=    "<div class=\"col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 \"></div>";
-                        break;
+                        if (data.hasOwnProperty(i)) {
+                            out += "<div class=\"cards\">";
+                            out += "<div class=\"col-md-4 col-lg-4 col-xl-4 mx-auto mt-3\">";
+                            out += "<div class=\"card\">";
+                            out += " <img src=\"img/" + data[i].preview + "\" class=\"card-img-top\" alt=\"...\">";
+                            out += "<div class=\"card-body\">";
+                            out += "<p class=\"card-text\">" + data[i].name + "</p>";
+                            out += "<p class=\"card-text money\">" + data[i].price + " грн.</p>";
+                            out += "<div class=\"buttons\">";
+                            out += " <a href='information.php?id=" + data[i].id + "'><input type=\"button\" class=\"btn sbtn\" value=\"Подробнее\" data-id=" + data[i].id + "' ></a>";
+                            out += "<div class=\"input-image\"><input type=\"image\" src=\"img/heart.png\" onclick='checkRegistration()'></div>";
+                            out += " </div>";
+                            out += "</div>";
+                            out += "</div>";
+                            out += "</div>";
+                            out += "<div class=\"col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 \"></div>";
+                            break;
+                        }
                     }
                     case 1: {
-                        out += "<div class=\"col-md-4 col-lg-4 col-xl-4 mx-auto mt-3\">";
-                        out +=  "<div class=\"card\">";
-                        out += "<img src=\"img/"+data[i].preview+"\" class=\"card-img-top\" alt=\"...\">";
-                        out += "<div class=\"card-body\">";
-                        out += "<p class=\"card-text\">"+data[i].name+"</p>";
-                        out += "<p class=\"card-text money\">"+data[i].price+" грн.</p>";
-                        out +="<div class=\"buttons\">";
-                        out +=" <a href='information.php?id="+data[i].id+"'><input type=\"button\" class=\"btn sbtn\" value=\"Подробнее\" data-id="+data[i].id+"' ></a>";
-                        out +="<div class=\"input-image\"><input type=\"image\" src=\"img/heart.png\" onclick='checkRegistration()'></div>";
-                        out +=" </div>";
-                        out += "</div>";
-                        out += "</div>";
-                        out += "</div>";
-                        out += "</div>";
-                        break;
+                        if (data.hasOwnProperty(i)) {
+                            out += "<div class=\"col-md-4 col-lg-4 col-xl-4 mx-auto mt-3\">";
+                            out += "<div class=\"card\">";
+                            out += "<img src=\"img/" + data[i].preview + "\" class=\"card-img-top\" alt=\"...\">";
+                            out += "<div class=\"card-body\">";
+                            out += "<p class=\"card-text\">" + data[i].name + "</p>";
+                            out += "<p class=\"card-text money\">" + data[i].price + " грн.</p>";
+                            out += "<div class=\"buttons\">";
+                            out += " <a href='information.php?id=" + data[i].id + "'><input type=\"button\" class=\"btn sbtn\" value=\"Подробнее\" data-id=" + data[i].id + "' ></a>";
+                            out += "<div class=\"input-image\"><input type=\"image\" src=\"img/heart.png\" onclick='checkRegistration()'></div>";
+                            out += " </div>";
+                            out += "</div>";
+                            out += "</div>";
+                            out += "</div>";
+                            out += "</div>";
+                            break;
+                        }
                     }
                     case 2: {
-                        out += "<div class=\"gridDiv\" style=\"margin-top: -30%;\">\n" +
-                                    "                <div class=\"alone-card\">\n" +
-                                    "                    <div class=\"col-md-4 col-lg-4 col-xl-4 mx-auto mt-3\">\n" +
-                                    "                        <div class=\"card\">\n" +
-                                    "                            <img src=\"img/"+data[i].preview+"\" class=\"card-img-top\" alt=\"...\">\n" +
-                                    "                            <div class=\"card-body\">\n" +
-                                    "                                <p class=\"card-text\">"+data[i].name+"</p>\n" +
-                                    "                                <p class=\"card-text money\">"+data[i].price+" грн.</p>";
-                        out +="<div class=\"buttons\">";
-                        out +=" <a href='information.php?id="+data[i].id+"'><input type=\"button\" class=\"btn sbtn\" value=\"Подробнее\" data-id="+data[i].id+"' ></a>";
-                        out +="<div class=\"input-image\"><input type=\"image\" src=\"img/heart.png\" onclick='checkRegistration()'></div>";
-                        out +=" </div>";
-                                   out += "                            </div>\n" +
-                                    "                        </div>\n" +
-                                    "                    </div>\n" +
-                                    "                </div>";
-                                break;
+                        if (data.hasOwnProperty(i)) {
+                            out += "<div class=\"gridDiv\" style=\"margin-top: -30%;\">\n" +
+                                "                <div class=\"alone-card\">\n" +
+                                "                    <div class=\"col-md-4 col-lg-4 col-xl-4 mx-auto mt-3\">\n" +
+                                "                        <div class=\"card\">\n" +
+                                "                            <img src=\"img/" + data[i].preview + "\" class=\"card-img-top\" alt=\"...\">\n" +
+                                "                            <div class=\"card-body\">\n" +
+                                "                                <p class=\"card-text\">" + data[i].name + "</p>\n" +
+                                "                                <p class=\"card-text money\">" + data[i].price + " грн.</p>";
+                            out += "<div class=\"buttons\">";
+                            out += " <a href='information.php?id=" + data[i].id + "'><input type=\"button\" class=\"btn sbtn\" value=\"Подробнее\" data-id=" + data[i].id + "' ></a>";
+                            out += "<div class=\"input-image\"><input type=\"image\" src=\"img/heart.png\" onclick='checkRegistration()'></div>";
+                            out += " </div>";
+                            out += "                            </div>\n" +
+                                "                        </div>\n" +
+                                "                    </div>\n" +
+                                "                </div>";
+                            break;
+                        }
                     }
                     case 3: {
-                        out += "<div class=\"another-cards cards\">\n" +
-                            "                    <div class=\"col-md-4 col-lg-4 col-xl-4 mx-auto mt-3\">\n" +
-                            "                        <div class=\"card\" style=\"top: 115px;\">\n" +
-                            "                            <img src=\"img/"+data[i].preview+"\" class=\"card-img-top\" alt=\"...\">\n" +
-                            "                            <div class=\"card-body\">\n" +
-                            "                                <p class=\"card-text\">"+data[i].name+"</p>\n" +
-                            "                                <p class=\"card-text money\">"+data[i].price+" грн.</p>";
-                        out +="<div class=\"buttons\">";
-                        out +=" <a href='information.php?id="+data[i].id+"'><input type=\"button\" class=\"btn sbtn\" value=\"Подробнее\" data-id="+data[i].id+"' ></a>";
-                        out +="<div class=\"input-image\"><input type=\"image\" src=\"img/heart.png\" onclick='checkRegistration()'></div>";
-                        out +=" </div>";
-                           out += "                            </div>\n" +
-                            "                        </div>\n" +
-                            "                    </div>\n" +
-                            "\n" +
-                            "                    <div class=\"col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 \"></div>";
+                        if (data.hasOwnProperty(i)) {
+                            out += "<div class=\"another-cards cards\">\n" +
+                                "                    <div class=\"col-md-4 col-lg-4 col-xl-4 mx-auto mt-3\">\n" +
+                                "                        <div class=\"card\" style=\"top: 115px;\">\n" +
+                                "                            <img src=\"img/" + data[i].preview + "\" class=\"card-img-top\" alt=\"...\">\n" +
+                                "                            <div class=\"card-body\">\n" +
+                                "                                <p class=\"card-text\">" + data[i].name + "</p>\n" +
+                                "                                <p class=\"card-text money\">" + data[i].price + " грн.</p>";
+                            out += "<div class=\"buttons\">";
+                            out += " <a href='information.php?id=" + data[i].id + "'><input type=\"button\" class=\"btn sbtn\" value=\"Подробнее\" data-id=" + data[i].id + "' ></a>";
+                            out += "<div class=\"input-image\"><input type=\"image\" src=\"img/heart.png\" onclick='checkRegistration()'></div>";
+                            out += " </div>";
+                            out += "                            </div>\n" +
+                                "                        </div>\n" +
+                                "                    </div>\n" +
+                                "\n" +
+                                "                    <div class=\"col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 \"></div>";
                             break;
+                        }
                     }
                     case 4: {
-                        out += "<div class=\"col-md-4 col-lg-4 col-xl-4 mx-auto mt-3\">\n" +
-                            "                        <div class=\"card\">\n" +
-                            "                            <img src=\"img/"+data[i].preview+"\" class=\"card-img-top\" alt=\"...\">\n" +
-                            "                            <div class=\"card-body\">\n" +
-                            "                                <p class=\"card-text\">"+data[i].name+"</p>\n" +
-                            "                                <p class=\"card-text money\">"+data[i].price+" грн.</p>";
-                        out +="<div class=\"buttons\">";
-                        out +=" <a href='information.php?id="+data[i].id+"'><input type=\"button\" class=\"btn sbtn\" value=\"Подробнее\" data-id="+data[i].id+"' ></a>";
-                        out +="<div class=\"input-image\"><input type=\"image\" src=\"img/heart.png\" onclick='checkRegistration()'></div>";
-                        out +=" </div>";
-                           out += "                            </div>\n" +
-                            "                        </div>\n" +
-                            "                    </div>\n" +
-                            "\n" +
-                            "                </div><!-- another-cards -->\n" +
-                            "\n" +
-                            "            </div>";
-                        break;
+                        if (data.hasOwnProperty(i)) {
+                            out += "<div class=\"col-md-4 col-lg-4 col-xl-4 mx-auto mt-3\">\n" +
+                                "                        <div class=\"card\">\n" +
+                                "                            <img src=\"img/" + data[i].preview + "\" class=\"card-img-top\" alt=\"...\">\n" +
+                                "                            <div class=\"card-body\">\n" +
+                                "                                <p class=\"card-text\">" + data[i].name + "</p>\n" +
+                                "                                <p class=\"card-text money\">" + data[i].price + " грн.</p>";
+                            out += "<div class=\"buttons\">";
+                            out += " <a href='information.php?id=" + data[i].id + "'><input type=\"button\" class=\"btn sbtn\" value=\"Подробнее\" data-id=" + data[i].id + "' ></a>";
+                            out += "<div class=\"input-image\"><input type=\"image\" src=\"img/heart.png\" onclick='checkRegistration()'></div>";
+                            out += " </div>";
+                            out += "                            </div>\n" +
+                                "                        </div>\n" +
+                                "                    </div>\n" +
+                                "\n" +
+                                "                </div><!-- another-cards -->\n" +
+                                "\n" +
+                                "            </div>";
+                            break;
+                        }
                     }
                 }
             }
